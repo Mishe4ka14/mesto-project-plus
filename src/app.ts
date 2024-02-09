76741983a29231c88a3acb51864e89d057ef70ca
Mishe4ka14 <mishechka.14@yandex.ru>
2024-02-09 22:22:59 +0300

    fix tsconfig, add models for cards and users

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,14 @@
 import express from 'express';
+import { MongoClient } from 'mongodb';
+
+
+/* Создаём экземпляр MongoClient, передав URL для подключения к MongoDB */
+const client = new MongoClient('mongodb://localhost:27017/mestodb');
+async function main() {
+  await client.connect();
+}
+
+main().catch(error => console.error(error));
 
 const {PORT = 3000} = process.env;
 
